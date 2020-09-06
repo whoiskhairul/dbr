f44e121fdf1e13b905ff922d51bc30def908485b
Khairul Islam <whoiskhairul@gmail.com>
2020-09-06 17:57:19 +0600

    security update

--- dbus/settings.py
+++ dbus/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm-vlrwgbu_8_x1$wxlb#(x%1j(hmu9)^-x!hbw1#rpbdlhjbuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- dbus/urls.py
+++ dbus/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('khairul/', admin.site.urls),
     path('', include('bus.urls')),
     path('map/', include('map.urls')),
     path('accounts/',include('allauth.urls')),
